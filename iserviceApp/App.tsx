654cec3f349166311f9982f1a4b58c02c1d55abf
LuizFelixDev <luizhenriquefelix138@gmail.com>
2026-04-04 08:07:56 -0300

    Adicionando navegação em pilha para nova screen

--- iserviceApp/App.tsx
+++ iserviceApp/App.tsx
@@ -1,18 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import Login from './src/Screens/Login/Login';
+import SingIn from './src/Screens/SingIn/SingIn';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <Login></Login>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="SingIn" component={SingIn} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
